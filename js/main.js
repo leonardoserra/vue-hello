c9752362566ab5c4e60536fa7a316fe2aad5cda8
leonardoserra <serra.leonardo92@gmail.com>
2023-03-14 13:17:42 +0100

    completato con bonus, ora stilizzo

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      src: 'https://digitalya.co/blog/wp-content/uploads/2022/08/vue-js-methods-demistified@3x-8-2048x667.png'
     }
   }
 }).mount('#app');
