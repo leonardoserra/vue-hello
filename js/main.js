9fdcec7fa6cdc854703776475bf4ab43a5a17018
leonardoserra <serra.leonardo92@gmail.com>
2023-03-14 13:35:37 +0100

    aggiunto style ma da vueJs BONUS FINE

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ createApp({
     return {
       message: 'Hello Vue!',
       src: 'https://digitalya.co/blog/wp-content/uploads/2022/08/vue-js-methods-demistified@3x-8-2048x667.png',
-      src2: 'img/img1.png'
+      src2: 'img/img1.png',
+      text: 'text-center',
+      size: 'smaller'
     }
   }
 }).mount('#app');
