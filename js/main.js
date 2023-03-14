7f62dc8a1471ab8972a7ccdbbedd78bd82a80cdc
leonardoserra <serra.leonardo92@gmail.com>
2023-03-14 13:30:27 +0100

    provato bonus con un img locale OK

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ createApp({
   data() {
     return {
       message: 'Hello Vue!',
-      src: 'https://digitalya.co/blog/wp-content/uploads/2022/08/vue-js-methods-demistified@3x-8-2048x667.png'
+      src: 'https://digitalya.co/blog/wp-content/uploads/2022/08/vue-js-methods-demistified@3x-8-2048x667.png',
+      src2: 'img/img1.png'
     }
   }
 }).mount('#app');
