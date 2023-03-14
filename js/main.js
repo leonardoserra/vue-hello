204d285baee2a2e1d9f2e6e41fac1968a6bdfbfa
leonardoserra <serra.leonardo92@gmail.com>
2023-03-14 16:52:02 +0100

    cambiato nome variabile src in imageVue

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ createApp({
   data() {
     return {
       message: 'Hello Vue!',
-      src: 'https://digitalya.co/blog/wp-content/uploads/2022/08/vue-js-methods-demistified@3x-8-2048x667.png',
-      src2: 'img/img1.png',
+      imageVueUrl: 'https://digitalya.co/blog/wp-content/uploads/2022/08/vue-js-methods-demistified@3x-8-2048x667.png',
+      imageVue: 'img/img1.png',
       text: 'text-center',
       size: 'smaller'
     }
